9c29c9f716a9c4f538a66dc5478640371be158e4
nicra <nicra@itu.dk>
2025-03-21 16:51:12 +0100

    Cleaner api

--- api.py
+++ api.py
@@ -48,34 +48,11 @@ def get_user(request: Request):
     
     return Response(status_code=404, message="User not found", data=None)
 
-# @app.get("/users/", response_model=List[Dict[int, User]])
-# def get_users():
-#     return [users_db]
-
-@app.get("/users/{user_id}", response_model=User)
-def get_user(user_id: int):
-    if user_id not in users_db:
-        raise HTTPException(status_code=404, detail="User not found")
-    return users_db[user_id]
-
 @app.get("/users/{email}", response_model=User)
 def get_user(user_email: str):
     if user_email not in users_db:
         raise HTTPException(status_code=404, detail="User not found")
     return users_db[user_email]
 
-# @app.put("/users/{user_id}", response_model=User)
-# def update_user(user_id: int, updated_user: User):
-#     if user_id not in users_db:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     users_db[user_id] = updated_user
-#     return updated_user
-
-# @app.delete("/users/{user_id}")
-# def delete_user(user_id: int):
-#     if user_id not in users_db:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     del users_db[user_id]
-#     return {"message": "User deleted successfully"}
 
 # To run the API, use: `uvicorn script_name:app --reload`
